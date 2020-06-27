22f781420d9b0671f9b721e51b00c3d6d0ddb26d
Moein <moein.shariatnia@gmail.com>
2020-06-27 13:22:12 +0430

    media added-Event model created

--- events/views.py
+++ events/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Moein',
-        'title': 'Post 1',
-        'content': 'First opportunity to go abroad!',
-        'date_posted': 'June 25, 2020'
-    },
-    {
-        'author': 'Hnaye',
-        'title': 'Post 2',
-        'content': 'Second opportunity to go abroad!',
-        'date_posted': 'June 26, 2020'
-    }
-]
-
 
 def home(request):
     context = {
-        'posts': posts,
+        'events': Event.objects.all(),
         'title': "Events"
     }
     return render(request, 'events/events_home.html', context)
--- sicoms/settings.py
+++ sicoms/settings.py
@@ -101,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -120,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
