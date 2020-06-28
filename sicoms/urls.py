bdd215cee399bb310574582c1f1877462806fb9b
Moein <moein.shariatnia@gmail.com>
2020-06-28 14:23:22 +0430

    login-logout-profile added/base html modified

--- sicoms/urls.py
+++ sicoms/urls.py
@@ -18,13 +18,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_view
+from events import views as event_view
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('events/', include('events.urls')),
+    path('events/', event_view.home, name="events-home"),
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path('', include('homepage.urls')),
-    path('register/', user_view.register, name='register')
+    path('register/', user_view.register, name='register'),
+    path('profile/', user_view.profile, name='profile')
 ]
 
 if settings.DEBUG:
