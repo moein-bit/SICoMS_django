a2c453d4359e425d51d6014dfd8b8f0b680a0c19
Moein <moein.shariatnia@gmail.com>
2020-06-29 20:17:07 +0430

    registeration form modefied

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,39 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
+    MAJORS = (
+        ('MED', "Medicine"),
+        ('OTH', 'Other'),
+    )
     email = forms.EmailField()             
+    first_name = forms.CharField(max_length=50, 
+                                 required=True, 
+                                 label="First Name")
+
+    last_name = forms.CharField(max_length=50, 
+                                required=True,
+                                label="Last Name")
+
+    major = forms.ChoiceField(choices=MAJORS, 
+                              label="Your Major",
+                              required=True)
+
+    semester = forms.IntegerField(label="Semester",
+                                  required=True)
+
+    student_id = forms.IntegerField(required=True,
+                                    label="Student ID")
+
+    university = forms.CharField(max_length=60,
+                                 label="University",
+                                 required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name',
+                  'last_name', 'major', 'semester', 
+                  'student_id', 'university', 
+                  'password1', 'password2']
 
 
 '''
--- users/views.py
+++ users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm#, ProfileInfoForm
+from django.contrib.auth import login, authenticate
 
-def register(request):
+'''def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         #profile_form = ProfileInfoForm(request.POST)
@@ -16,7 +17,31 @@ def register(request):
     else:
         form = UserRegisterForm()
         #profile_form = ProfileInfoForm()
+    return render(request, 'users/register.html', {'form': form})'''
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.profile.major = form.cleaned_data.get('major')
+            user.profile.semester = form.cleaned_data.get('semester')
+            user.profile.university = form.cleaned_data.get('university')
+            user.profile.student_id = form.cleaned_data.get('student_id')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            user = authenticate(username=username, password=password, email=email)
+            messages.success(request, f"Dear {user.profile.first_name}, Your account has been created! Log in by your username: {username}")
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
